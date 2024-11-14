2c1e53638045908d666406ac86e598751669e0bd
SajestSimon <150657700+SajestSimon@users.noreply.github.com>
2024-11-14 18:06:39 +0100

    aa
    sss

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public String? HellloStr { get; set; }
+        public Class1(String? name)
+        {
+            HellloStr = name;
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-t
+using ConsoleApp1;
+using System.Xml.Linq;
+
+Class1 dude = new Class1("    ");
+if (!string.IsNullOrWhiteSpace(dude.HellloStr)) { Console.WriteLine("Hello " + dude.HellloStr); }
+
+else { Console.WriteLine("sorry -  no real input"); }
+
